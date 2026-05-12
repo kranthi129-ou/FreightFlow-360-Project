56a71e9c32ed222a2bc7e0a28411cb762ce63207
kranthiteja129 <kranthi.teja.boddam-1@ou.edu>
2026-05-12 12:00:12 -0500

    Remove create admin link

--- frontend/src/app/pages/admin-login/admin-login.ts
+++ frontend/src/app/pages/admin-login/admin-login.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { PublicNavbar } from '../../shared/public-navbar/public-navbar';
 import { AdminAuthService } from '../../services/admin-auth.service';
 
@@ -11,13 +11,12 @@ import { AdminAuthService } from '../../services/admin-auth.service';
   imports: [
     CommonModule,
     FormsModule,
-    RouterLink,
     PublicNavbar
   ],
   templateUrl: './admin-login.html',
   styleUrl: './admin-login.css'
 })
-export class AdminLogin implements OnInit {
+export class AdminLogin {
   email = 'visitor@freightflow360.com';
   password = 'Visiter@123';
   errorMessage = '';
@@ -27,19 +26,6 @@ export class AdminLogin implements OnInit {
     private adminAuthService: AdminAuthService,
     private router: Router
   ) {}
-
-  ngOnInit(): void {
-    this.adminAuthService.setupStatus().subscribe({
-      next: (response) => {
-        if (response.setupRequired) {
-          this.router.navigate(['/admin-setup']);
-        }
-      },
-      error: () => {
-        this.errorMessage = 'Unable to create admin account.';
-      }
-    });
-  }
 
   login(): void {
     this.errorMessage = '';
